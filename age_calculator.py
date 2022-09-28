e5dd8fc26ccb8afb80db4b1638965d97ca7741ba
Dalal Alsabah <dalalalsabah@Dalals-MacBook-Pro.local>
2022-09-29 01:52:19 +0300

    task one error resolved and tested

--- age_calculator.py
+++ age_calculator.py
@@ -2,15 +2,16 @@ from datetime import date
 
 
 def get_dob():
-	birth_year= input('enter your birth year:')
-	birth_month= input('enter your birth month:')
-	birth_day= input('enter birth day:')
+	birth_year= int(input('enter your birth year:'))
+	birth_month= int(input('enter your birth month:'))
+	birth_day= int(input('enter birth day:'))
 	import datetime as dt 
 	date=dt.date(birth_year,birth_month,birth_day)
 	return date
 
     # write code here
-	...
+	
+print (get_dob())
 
 
 def get_age(dob):
