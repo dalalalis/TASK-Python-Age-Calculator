9c1b13e6f45d252f3519622b50102b3e489e1bd1
Dalal Alsabah <dalalalsabah@Dalals-MacBook-Pro.local>
2022-09-29 01:49:40 +0300

    solved first task

--- age_calculator.py
+++ age_calculator.py
@@ -2,6 +2,13 @@ from datetime import date
 
 
 def get_dob():
+	birth_year= input('enter your birth year:')
+	birth_month= input('enter your birth month:')
+	birth_day= input('enter birth day:')
+	import datetime as dt 
+	date=dt.date(birth_year,birth_month,birth_day)
+	return date
+
     # write code here
 	...
 
