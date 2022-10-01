a09fbe9c469f17228f1d8f74af9daca5176c42de
Dalal Alsabah <dalalalsabah@Dalals-MacBook-Pro.local>
2022-10-01 18:33:51 +0300

    ll code written need to be checked

--- age_calculator.py
+++ age_calculator.py
@@ -1,27 +1,45 @@
-from datetime import date
 
 
+import datetime as dt 
 def get_dob():
 	birth_year= int(input('enter your birth year:'))
 	birth_month= int(input('enter your birth month:'))
-	birth_day= int(input('enter birth day:'))
-	import datetime as dt 
-	date=dt.date(birth_year,birth_month,birth_day)
-	return date
+	birth_day= int(input('enter birth day:')) 
+	dob=dt.date(birth_year,birth_month,birth_day)
+	print(dob)
+	return (dob.year)
 
     # write code here
-	
-print (get_dob())
+#get_dob()
+
+
 
 
 def get_age(dob):
-    # write code here
-	...
+	import datetime
+	today= datetime.date.today()
+	age= today.year -dob.year
+	
+	return (age)
+
+	
+#(get_age(get_dob()))
 
 
+
+# write code here
 def main():
-	# write code here
-	...
+	import datetime
+	date_year= datetime.date.today().year
+	dob=get_dob()
+	age= get_age(dob)
+	if get_dob.year < date_year:
+		return (age)
+	else:
+		print ("invalid date of birth")
+
+	
+
 
 
 if __name__ == '__main__':
