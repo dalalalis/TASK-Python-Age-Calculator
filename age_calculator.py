b65bf21dcbd1e724e088cd62919ab15f4dcb6af3
Dalal Alsabah <dalalalsabah@Dalals-MacBook-Pro.local>
2022-10-02 01:46:32 +0300

    fixed all error, works well

--- age_calculator.py
+++ age_calculator.py
@@ -1,14 +1,14 @@
+from datetime import date 
 
+def get_dob(): 
+	birth_year= int(input('enter your birth year:'))
+	birth_month= int(input('enter your birth month:'))
+	birth_day= int(input('enter birth day:'))
+	import datetime as dt 
+	date=dt.date(birth_year,birth_month,birth_day)
+	print (date)
+	return (birth_year)
 
-import datetime 
-def get_dob():
-	birth_year= input("which year were you born?")
-	birth_month=input("which month were you born ?")
-	birth_day=input("in which day of the month were you bonrn?")
-	dob=datetime.date(birth_year,birth_month,birth_day)
-	print(dob)
-	dob=dob.year
-	return (dob)
 
     # write code here
 #get_dob()
@@ -17,28 +17,25 @@ def get_dob():
 
 
 def get_age (dob):
-	dob-int(dob)
 	import datetime
 	today= datetime.date.today()
 	age= today.year - dob
-	print (age)
+	return (age)
 
 	
-
+#print (get_age(get_dob()))
 
 
 # write code here
 def main():
 	import datetime
-	dob=get_dob()
-	age=get_age(dob) 
-	date_year= datetime.date.year.today()
-	if dob< date_year:
-		return (age)
+	age = get_age(get_dob())
+	age1= int(age)
+	if age1 >=1:
+		return age
 	else:
-		print ("invalid date of birth")
+		return ("invalid date of birth")
 
-	
 print (main())
 
 
